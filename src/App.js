70137bb752634ac97f460aaf4e10e55a7245b70a
WanyonyiKenya <brianwanyonyike@gmail.com>
2022-12-07 10:07:41 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import QRCode from 'qrcode.react';
+import './App.css'
 
 function App() {
+  const [url, setUrl] = React.useState('');
+
+  function handleChange(e) {
+    setUrl(e.target.value);
+    setUrl('')
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault()
+      setUrl(e.target.value)
+      setUrl('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"
+       value={url}
+      //  hidden = {true}
+       onChange={handleChange}
+       onSubmit = {handleSubmit}
+      
+      />
+      <QRCode value={url} />
     </div>
   );
 }
